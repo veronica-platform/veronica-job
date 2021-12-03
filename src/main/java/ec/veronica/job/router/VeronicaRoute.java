27fead862c918f25e320fe1c20eb115eb2e95fd5
rolandopalermo <support@rolandopalermo.com>
2021-12-03 09:06:44 -0500

    Complete QA stage

--- src/main/java/ec/veronica/job/router/VeronicaRoute.java
+++ src/main/java/ec/veronica/job/router/VeronicaRoute.java
@@ -3,16 +3,21 @@ package ec.veronica.job.router;
 import ec.veronica.job.processor.FileProcessor;
 import lombok.Builder;
 import lombok.extern.slf4j.Slf4j;
+import lombok.var;
 import org.apache.camel.builder.RouteBuilder;
 
 import java.util.Optional;
 
+import static com.rolandopalermo.facturacion.ec.common.types.SriStatusType.STATUS_APPLIED;
 import static com.rolandopalermo.facturacion.ec.common.types.SriStatusType.STATUS_INTERNAL_ERROR;
+import static com.rolandopalermo.facturacion.ec.common.types.SriStatusType.STATUS_NOT_APPLIED;
 import static com.rolandopalermo.facturacion.ec.common.types.SriStatusType.STATUS_PENDING;
+import static ec.veronica.job.commons.DestinationFolder.FOLDER_AUTHORIZED;
 import static ec.veronica.job.commons.DestinationFolder.FOLDER_INBOX;
 import static ec.veronica.job.commons.DestinationFolder.FOLDER_PENDING;
 import static ec.veronica.job.commons.DestinationFolder.FOLDER_PROCESSING_ERROR;
 import static ec.veronica.job.commons.DestinationFolder.FOLDER_REJECTED;
+import static ec.veronica.job.commons.DestinationFolder.FOLDER_UNAUTHORIZED;
 import static ec.veronica.job.commons.FolderUtils.buildFolderPath;
 import static java.lang.String.format;
 import static java.util.Arrays.asList;
@@ -28,18 +33,24 @@ public class VeronicaRoute extends RouteBuilder {
     @Override
     public void configure() {
         log.debug("Registering route for folder: [{}]", rootFolder);
-        String endpointFolder = buildFolderPath(asList(rootFolder, FOLDER_INBOX.getValue()), Optional.empty());
-        Optional<String> fileName = Optional.of("fileName=${header.fileName}.xml");
+        var endpointFolder = buildFolderPath(asList(rootFolder, FOLDER_INBOX.getValue()), Optional.empty());
+        var xmlFileName = Optional.of("fileName=${header.fileName}.xml");
+        var pdfFileName = Optional.of("fileName=${header.fileName}.pdf");
         from(endpointFolder)
                 .routeId(routeId)
                 .process(fileProcessor)
                 .choice()
                     .when().simple(is(STATUS_PENDING.getValue()))
-                        .to(buildFolderPath(asList(rootFolder, FOLDER_PENDING.getValue()), fileName))
+                        .to(buildFolderPath(asList(rootFolder, FOLDER_PENDING.getValue()), xmlFileName))
                     .when().simple(is(STATUS_INTERNAL_ERROR.getValue()))
-                        .to(buildFolderPath(asList(rootFolder, FOLDER_PROCESSING_ERROR.getValue()), fileName))
+                        .to(buildFolderPath(asList(rootFolder, FOLDER_PROCESSING_ERROR.getValue()), xmlFileName))
                     .when().simple(is(STATUS_INTERNAL_ERROR.getValue()))
-                        .to(buildFolderPath(asList(rootFolder, FOLDER_REJECTED.getValue()), fileName))
+                        .to(buildFolderPath(asList(rootFolder, FOLDER_REJECTED.getValue()), xmlFileName))
+                    .when().simple(is(STATUS_NOT_APPLIED.getValue()))
+                        .setBody(simple("${header.appliedInvoice}")).toD(buildFolderPath(asList(rootFolder, FOLDER_UNAUTHORIZED.getValue()), xmlFileName))
+                    .when().simple(is(STATUS_APPLIED.getValue()))
+                        .setBody(simple("${header.appliedInvoice}")).toD(buildFolderPath(asList(rootFolder, FOLDER_AUTHORIZED.getValue(), "${header.folderName}"), xmlFileName))
+                        .setBody(simple("${header.ride}")).toD(buildFolderPath(asList(rootFolder, FOLDER_AUTHORIZED.getValue(), "${header.folderName}"), pdfFileName))
         ;
     }
 
